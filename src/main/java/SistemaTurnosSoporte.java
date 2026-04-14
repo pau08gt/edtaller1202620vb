9f821ca8fa9e9129e59d87cc236096c1fcc329bf
Paulo Guerra <paulo.guerra.teran@udla.edu.ec>
2026-04-14 13:00:34 -0500

    template estructuras

---
+++ src/main/java/SistemaTurnosSoporte.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SistemaTurnosSoporte {
+
+    private Queue<String> cola;
+    private static final int CAPACIDAD_MAXIMA = 10;
+
+    public SistemaTurnosSoporte() {
+
+    }
+
+    public boolean registrarTurno(String codigo, String problema) {
+
+    }
+
+    public String verSiguienteTurno() {
+
+    }
+
+    public String atenderSiguienteTurno() {
+
+    }
+
+    public int obtenerCantidadTurnos() {
+
+    }
+
+    public int obtenerEspaciosDisponibles() {
+
+    }
+
+    public String mostrarCola() {
+
+    }
+}
